9b5855ff41c216d3759c5c9b5865e98b0968011c
Mark Gilcrease <mgilcrease@gmail.com>
2018-10-27 16:53:41 -0500

    Adding babel config to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,34 +5,57 @@ const ManifestPlugin = require("webpack-manifest-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-	entry: {
-		app: path.resolve(__dirname, "public", "scripts", "index.js")
-	},
-	output: {
-		path: path.resolve(__dirname, "public", "dist"),
-		filename: "[name].bundle.js"
-	},
-	module: {
-		rules: [
-			{ test: /\.html$/, use: "html-loader" },
-			{ test: /\.txt$/, use: "raw-loader" },
-			{ test: /\.css$/, use: ["style-loader", "css-loader"] },
-			{ 
-				test: /\.(png|svg|jpg|gif|ico)$/i, 
-				loader: "file-loader?name=[name].[ext]"
-				//use: "file-loader" 
-			},
-			{ test: /\.(woff|wof2|eot|ttf|otf)$/, use: "file-loader" }
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({ title: "Output Management", template: path.resolve(__dirname, "public", "index.html") }),
-		new CleanWebpackPlugin(['dist']),
-		new ManifestPlugin()
-	],
-	mode: "development",
-	devtool: "inline-source-map",
-	devServer: {
-		contentBase: "./public/dist"
-	}
+    entry: {
+        app: path.resolve(__dirname, "public", "scripts", "index.js")
+    },
+    output: {
+        path: path.resolve(__dirname, "public", "dist"),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [{
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.txt$/,
+                use: "raw-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/i,
+                loader: "file-loader?name=[name].[ext]"
+            },
+            {
+                test: /\.(woff|wof2|eot|ttf|otf)$/,
+                use: "file-loader"
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+            template: path.resolve(__dirname, "public", "index.html")
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new ManifestPlugin()
+    ],
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: "./public/dist"
+    }
 }
